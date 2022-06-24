31fc6a175ea8c6a8677d6956fba18e60ce525d28
fravila08 <105952966+fravila08@users.noreply.github.com>
2022-06-23 23:17:32 -0700

    COMP

--- pokemon_project/pokemon_app/views.py
+++ pokemon_project/pokemon_app/views.py
@@ -1,5 +1,6 @@
 
 import json
+import random
 from random import randint
 from webbrowser import get
 from django.shortcuts import render  
@@ -13,13 +14,24 @@ def index(request):
 
 def pokepics(request):
    pokemon_id=request.POST.get("pokemon_id")
-   print(pokemon_id)
    endpoint = f"https://pokeapi.co/api/v2/pokemon/{pokemon_id}"
    response = requests.get(endpoint)
    responseJSON=response.json()
    picture={
       "pic":responseJSON["sprites"]["front_default"]
       }
+   type_name=responseJSON["types"][0]["type"]["name"]
+   other_end_point=f"https://pokeapi.co/api/v2/type/{type_name}"
+   response2=requests.get(other_end_point)
+   responseJSON2=response2.json()
+   i=0
+   while i<5:
+      id=random.randint(0,(len(responseJSON2["pokemon"])-1))
+      last_end_point=responseJSON2["pokemon"][id]["pokemon"]["url"]
+      last_response=requests.get(last_end_point)
+      last_responseJSON=last_response.json()
+      picture.update({"pic"+str(i):last_responseJSON["sprites"]["front_default"]})
+      i+=1
    return render(request, 'pages/pokemon.html', picture)
 
 
